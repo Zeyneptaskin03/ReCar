466325228320d8b1299e5851458c9c8594a64960
Zeynep Taşkın <77525793+Zeyneptaskin03@users.noreply.github.com>
2021-02-26 22:58:49 +0300

    Add files via upload

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,44 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete;
+using System;
+
+
+namespace ConsoleUI
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                CarTest();
+               
+               // ColorTest();
+            }
+        }
+
+        private static void CarTest()
+        {
+            CarManager carManager = new CarManager(new EfCarDal());
+            var result = carManager.GetCarDetails();
+
+            if (result.Success == true)
+            {
+                foreach (var car in result.Data)
+                {
+                    Console.WriteLine(car.CarId);
+                    Console.WriteLine(car.BrandId);
+                    Console.WriteLine(car.ColorId);
+                    Console.WriteLine(car.DailyPrice); 
+                    Console.WriteLine(car.Description);
+                    Console.WriteLine("............");
+                }
+            }
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
+        }
+
+        
+    }
+}
