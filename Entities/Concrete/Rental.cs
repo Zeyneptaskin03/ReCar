a9e77aca2d082d386dd3755afb63cb7df340c84a
Zeynep Taşkın <77525793+Zeyneptaskin03@users.noreply.github.com>
2021-02-26 23:04:52 +0300

    Add files via upload

---
+++ Entities/Concrete/Rental.cs
@@ -0,0 +1,17 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Rental : IEntity
+    {
+        public int Id { get; set; }
+        public int CarId { get; set; }
+        public int CustomerId { get; set; }
+        public DateTime RentDate { get; set; }
+        public DateTime ReturnDate { get; set; } //? var mı
+        //Araba teslim edilmemişse ReturnDate null'dır.
+    }
+}
---
+++ Entities/DTOs/CarDetailDto.cs
@@ -0,0 +1,23 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.DTOs
+{
+    public class CarDetailDto : IDto
+    {
+        /*CarName, BrandName, ColorName, DailyPrice. 
+         * 
+         *(İpucu : IDto oluşturup 3 tabloya join yazınız)*/
+        public int CarId { get; set; }
+        public int BrandId { get; set; }
+        public int ColorId { get; set; }
+        public int ModelYear { get; set; }
+        public int DailyPrice { get; set; }
+        public string Description { get; set; }
+        public string CarName { get; set; }
+        public string BrandName { get; set; }
+        public string ColorName { get; set; }
+    }
+}
